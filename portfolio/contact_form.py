358c3c32dd73803e8f967c6948876ee27da6c659
Oleg Sloviov <osoloviov@mail.ru>
2017-02-26 17:30:31 +0400

    подключил mailgun для отправки почты на osoloviov@list.ru, проверил - работает

--- portfolio/contact_form.py
+++ portfolio/contact_form.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.core.mail import send_mail
+#from django.core.mail import send_mail
+import requests
 
 
 class ContactForm(forms.Form):
@@ -8,8 +9,11 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message...'}))
 
     def send_email(self):
-        recipients = ['osoloviov@list.ru',]
-        send_mail(self.cleaned_data['subject'],
-                  self.cleaned_data['message'],
-                  self.cleaned_data['email'],
-                  recipients, fail_silently = False)
+        return requests.post(
+            "https://api.mailgun.net/v3/sandbox075b55521f59465c82d4d87856d6f43c.mailgun.org/messages",
+            auth=("api", "key-e1518fd3e6d897d250e23581f295417c"),
+            data={"from": self.cleaned_data['email'],
+                  "to": "Oleg <osoloviov@list.ru>",
+                  "subject": self.cleaned_data['subject'],
+                  "text": self.cleaned_data['message']})
+
