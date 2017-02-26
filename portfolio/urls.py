67128ce7a84c75c482ea4e06669177b446b6238b
Oleg Sloviov <osoloviov@mail.ru>
2017-02-26 12:21:37 +0400

    не все вошло в предыдущий коммит

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from portfolio.views import ContactView
 
 """
 Function views
@@ -13,4 +14,6 @@ Including another URLconf
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="portfolio/home_page.html"), name="home"),
+    url(r'^contact/$', ContactView.as_view(), name="contact"),
+    url(r'^thanks/$', TemplateView.as_view(template_name="portfolio/thanks.html"), name="thanks")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from portfolio.contact_form import ContactForm
+from django.views.generic.edit import FormView
+
+class ContactView(FormView):
+    template_name = 'portfolio/contact.html'
+    form_class = ContactForm
+    success_url = "/thanks/"#reverse('prtfolio:contact')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(ContactView, self).form_valid(form)
