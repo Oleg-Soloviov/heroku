178b8d8a418db9fd4806c9be085dddb5139a79c2
Oleg Sloviov <osoloviov@mail.ru>
2017-03-06 21:11:29 +0400

    теперь в зависимости от того установлена ли глобальная переменная DEBUG будет устанавливаться django.settings.DEBUG, т.е. просто DEBUG = os.environ.get('DEBUG', True), в heroku установил переменную DEBUG=False через heroku config:set

--- my_proj/settings.py
+++ my_proj/settings.py
@@ -26,7 +26,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'w-i9r*+qj$!hfom7$f#qcqmnfq5v+e*&@nx5(v(2k4mi)-3f3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
