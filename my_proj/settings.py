cd7533ddfaf1656edf1c96a5f5e6110d814858e0
Oleg Sloviov <osoloviov@mail.ru>
2017-03-07 00:13:45 +0400

    переключение  все таки работает, только надо открывать новую страницу

--- my_proj/settings.py
+++ my_proj/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',    # ADDED BY ME FOR I18N
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',    # ADDED BY ME FOR I18N
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
