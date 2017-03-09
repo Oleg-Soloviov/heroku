fbf1a8bf06c7f8ebd9b7a3ec61689038ad66047d
Oleg Sloviov <osoloviov@mail.ru>
2017-03-09 21:19:16 +0400

    поправил немного формы и перевод

--- my_proj/settings.py
+++ my_proj/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'crispy_forms',
-    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
