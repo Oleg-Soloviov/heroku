bd33e7b2537179805c4f7fec2e35db8dfc359a38
Oleg Sloviov <osoloviov@mail.ru>
2017-03-12 17:26:56 +0400

    поправил немного стили и перевод

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,7 @@ app_name = 'portfolio'
 urlpatterns = [
     url(r'^contact/$', ContactView.as_view(), name="contact"),
     url(r'^thanks/$', TemplateView.as_view(template_name="portfolio/thanks.html"), name="thanks"),
+    url(r'^my_style_home_page/$', TemplateView.as_view(template_name="portfolio/my_style_home_page.html"), name="my_style_homepage"),
     url(r'^django-forms/$', AllDjangoFieldsView.as_view(), name="django_forms"),
     url(r'^$', TemplateView.as_view(template_name="portfolio/home_page.html"), name="home"),
 ]
