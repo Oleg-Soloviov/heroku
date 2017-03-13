19e40c6bf2e18726196aa50404b6dd1d323fac4e
Oleg Sloviov <osoloviov@mail.ru>
2017-03-13 20:24:15 +0400

    переработал django fields form - теперь будут раздельные формы, TextInput почти готов

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from portfolio.views import ContactView, AllDjangoFieldsView
+from .views import ContactView, AllDjangoFieldsView, django_fields
 
 
 app_name = 'portfolio'
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^contact/$', ContactView.as_view(), name="contact"),
     url(r'^thanks/$', TemplateView.as_view(template_name="portfolio/thanks.html"), name="thanks"),
     url(r'^my_style_home_page/$', TemplateView.as_view(template_name="portfolio/my_style_home_page.html"), name="my_style_homepage"),
-    url(r'^django-forms/$', AllDjangoFieldsView.as_view(), name="django_forms"),
+    #url(r'^django-forms/$', AllDjangoFieldsView.as_view(), name="django_forms"),
+    url(r'^django-forms/$', django_fields, name="django_forms"),
     url(r'^$', TemplateView.as_view(template_name="portfolio/home_page.html"), name="home"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
-
+from django.shortcuts import render
 from django.views.generic.edit import FormView
 from portfolio.contact_form import ContactForm
-from portfolio.forms import AllDjangoFieldsForm
+from .forms import AllDjangoFieldsForm, TextInputFieldsForm
 
 
 class ContactView(FormView):
@@ -21,3 +21,26 @@ class AllDjangoFieldsView(FormView):
     template_name = 'portfolio/django_forms.html'
     form_class = AllDjangoFieldsForm
     success_url = reverse_lazy('portfolio:django_forms')
+
+
+
+
+
+def django_fields(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        textinputform = TextInputFieldsForm(request.POST)
+        # check whether it's valid:
+        if textinputform.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+            pass
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        textinputform = TextInputFieldsForm()
+
+    return render(request, 'portfolio/django_forms.html', {'textinputform': textinputform})
