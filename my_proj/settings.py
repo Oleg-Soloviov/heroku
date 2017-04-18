a3ef6285f5f3f72ad6cae875424ab3349e623282
Oleg Sloviov <osoloviov@mail.ru>
2017-04-18 14:33:02 +0400

    debug21

--- my_proj/settings.py
+++ my_proj/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'portfolio.apps.PortfolioConfig',
+    'portfolio',
     'crispy_forms',
     'tinymce',
 #    'django_wysiwyg',
